31fc65c6e34f2c8b6ad6ffd337e82092f735b0fb
RenatoMarin <61033115+RenatoMarin@users.noreply.github.com>
2020-06-10 02:17:56 -0400

    Add files via upload

---
+++ repositories/hospital-repo-array.js
@@ -0,0 +1,24 @@
+class HospitalRepositoryArray {
+    
+    constructor(){
+        this.hospitais = []
+    }
+
+    cadastrarHospital(hospital){
+        this.hospitais.push(hospital)
+    }
+    excluirHospital(id){
+
+    }
+    alterarHospital(hospital){
+
+    }
+    buscarHospital(hospital){
+
+    }
+    buscarTodosHospitais(){
+        return this.hospitais;
+    }
+}
+
+module.exports = HospitalRepositoryArray
